9b6d64ee05bf50726180b7eef9cd6930937791df
Eliseu Vicente <eliseu965@gmail.com>
2023-09-23 14:12:49 -0300

    insert styles for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './global.css'
 import { Header } from './components/Header';
-import styles from './App.module.css';
 import { NewTask } from './components/NewTask';
-import { Task } from './components/Task';
-
 
 
 export function App(){
